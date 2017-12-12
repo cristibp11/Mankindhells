f7eab49269547cccc5203164103d274b04f6b618
cristibp11 <32294283+cristibp11@users.noreply.github.com>
2017-12-12 13:44:19 +0100

    Creating testing branch.

---
+++ B106_Lofify_TESTING-0.0.1-SNAPSHOT/src/main/java/B1_06/B106_Lofify_TESTING/App.java
@@ -0,0 +1,13 @@
+package B1_06.B106_Lofify_TESTING;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
